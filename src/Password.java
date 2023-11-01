657d4507518765f174fa47c7712286891db6ec54
Viggo2102 <viktorrasmussen@Viktors-MacBook-Air.local>
2023-11-01 12:05:52 +0100

    Klasse til håndtering af password

---
+++ src/Password.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class Password {
+        static String passwords;
+        static String a;
+
+
+        public static boolean password() {
+
+            Scanner scanner = new Scanner(System.in);
+
+            passwords = "hairyharry";
+            a = "";
+
+            System.out.println("Indtast password");
+            System.out.println("Tast b for at afslutte");
+
+            while (!a.equals(passwords)) {
+                a = scanner.nextLine();
+                if (a.equals("hairyharry")) {
+                    System.out.println("Velkommen ind");
+                    return true;
+
+                } else if (a.equals("b")) {
+                    System.out.println("Farvel");
+                    return  false;
+
+                } else {
+                    System.out.println("Forkert password");
+
+                }
+
+            }
+            return false;
+        }
+}
